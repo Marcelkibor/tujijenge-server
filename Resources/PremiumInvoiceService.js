6d3f48752dd2518ccb61d932fe8507794123463f
Marcelkibor <kibormarcel90@gmail.com>
2024-03-21 10:48:13 +0300

    pdf layout section 2

--- Resources/PremiumInvoiceService.js
+++ Resources/PremiumInvoiceService.js
@@ -6,6 +6,13 @@ const PremiumInvoiceService = {
             training:{item:{name:'Training',x:50,y:300},rate:{val:0.0,x:260,y:300},price:{val:500,x:460,y:300}},
             premium:{item:{name:'Premium Membership',x:50,y:275},rate:{val:0.0,x:260,y:275},price:{val:1500,x:460,y:275}}
         }
+        let total = premiumBulk.premium.price.val+premiumBulk.training.price.val
+        const cummulative = {  
+            subtotal:{name:{title:'Subtotal',x:390,y:390},values:{total:total,x:450,y:390}},
+            discount:{name:{title:'Discount',x:390,y:410},values:{total:0.0,x:450,y:410}},
+            tax:{name:{title:'Tax',x:390,y:430},values:{total:0.0,x:450,y:430}},
+            total:{name:{title:'Total',x:390,y:450},values:{total:total,x:450,y:450}}
+        }
         try {
             //x-axis,y-axis
             const doc = new premiumInvoicePdf();
@@ -32,14 +39,14 @@ const PremiumInvoiceService = {
             let cenY = invoiceDiv.y+invoiceDiv.height/2
             doc.fillColor('white');
             doc.text('Invoice',cenX-30,cenY-6);
-            // Write bill from
+            // Write bill details
             doc.fontSize(14).fillColor('black').font('Helvetica-Bold').text('Bill From:', 50, 120);
             doc.font('Helvetica').fillColor('black').fontSize(12).text(`Reduced Poverty Club`,  50,140);
 
             doc.fontSize(14).fillColor('black').font('Helvetica-Bold').text('Bill To:', 240, 120);
-            doc.font('Helvetica').fillColor('black').fontSize(12).text(`Name: ${data.clientName}\nPhone:${data.phone}`, 240,140);
+            doc.font('Helvetica').fillColor('black').fontSize(12).text(`${data.clientName}\n${data.phone}`, 240,140);
             doc.fontSize(14).fillColor('black').font('Helvetica-Bold').text(`Invoice No:`, 420, 120);
-            doc.font('Helvetica').fillColor('black').fontSize(12).text(`${data.iNumber}\nDate: ${data.due}\nDue: ${data.due}`, 420,140);
+            doc.font('Helvetica').fillColor('black').fontSize(12).text(`${data.iNumber}\nDate: ${data.due}\nDue:  ${data.due}`, 420,140);
             
             doc.fillColor('#d66016').rect(splitDiv.x,splitDiv.y,splitDiv.width,splitDiv.height).fill();
             doc.fillColor('white').font('Helvetica-Bold').text('Item',splitDiv.x+50,splitDiv.y+10);
@@ -54,9 +61,18 @@ const PremiumInvoiceService = {
             // item.price
             doc.fillColor('black').font('Helvetica').text(premiumBulk.premium.price.val,premiumBulk.premium.price.x-10,premiumBulk.premium.price.y).fill();
             doc.fillColor('black').font('Helvetica').text(premiumBulk.training.price.val,premiumBulk.training.price.x-7,premiumBulk.training.price.y).fill();
+            //totals
+            doc.text(cummulative.subtotal.name.title,cummulative.subtotal.name.x,cummulative.subtotal.name.y);
+            doc.text(cummulative.subtotal.values.total,cummulative.subtotal.values.x,cummulative.subtotal.values.y);
+            doc.text(cummulative.discount.name.title,cummulative.discount.name.x,cummulative.discount.name.y);
+            doc.text(cummulative.discount.values.total,cummulative.discount.values.x,cummulative.discount.values.y);
+            doc.text(cummulative.tax.name.title,cummulative.tax.name.x,cummulative.tax.name.y);
+            doc.text(cummulative.tax.values.total,cummulative.tax.values.x,cummulative.tax.values.y);
+            doc.font('Helvetica-Bold').fontSize(18).text(cummulative.total.name.title,cummulative.total.name.x,cummulative.total.name.y);
+            doc.text(cummulative.total.values.total,cummulative.total.values.x,cummulative.total.values.y);
+            doc.font('Helvetica').fontSize(14).text('Thank you for purchasing this premium subsciption. For more information or any inquiries please contact 0712345677',50,650)
+            // finalize
             doc.pipe(res);
-
-            // Finalize the PDF
             doc.end();
         } catch (error) {
             console.error(`Error generating PDF: ${error}`);
